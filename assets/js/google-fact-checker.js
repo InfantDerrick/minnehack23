a7501063f778d639edff0648f8e1d2de83046d1a
jeevanprakash0814 <jeevanpraks@gmail.com>
2023-01-29 13:49:30 -0600

    Completed API calls for fact checker

--- assets/js/google-fact-checker.js
+++ assets/js/google-fact-checker.js
@@ -1,8 +1,8 @@
-function getFactCheck(query, callback = (printing) => {console.log(printing);
-    makeFactCheck(printing);}) {
+function getFactCheck(query, callback = (printing) => {makeFactCheck(printing);}) {
     // fetch(`https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en-US&maxAgeDays=100&offset=0&query=${query}`)
     //     .then((response)) => response.json())
     //     .then((json) => console.log(json));
+    console.log(query);
     theUrl = `https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en-US&maxAgeDays=100&offset=0&query=${query}&key=AIzaSyBC0im9IZ8UfIFW9AkdW6q41XpCfL9z-CM`
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
@@ -14,13 +14,29 @@ function getFactCheck(query, callback = (printing) => {console.log(printing);
 }
 
 const makeFactCheck = (checks) => {
-    console.log(checks);
     const facts = JSON.parse(checks);
-    console.log(facts);
+    var arr = [];
     if(facts['claims']){
         facts.claims.forEach((x) => {
+            arr.push(x.claimReview[0].textualRating);
             console.log(x.claimReview[0].textualRating);
         });
     }
     
+    var score = 0;
+    var counter = 0;
+    for(rating in arr){
+        counter = counter + 1;
+        if(rating.includes("true"))
+            score = score + 1;
+        else if(rating.includes("false"))
+            score = score;
+        else if(!rating.includes("false"))
+            score = score + 0.5;
+    }
+
+    score = parseFloat(score) / counter;
+    score = score * 100;
+    document.getElementById("reply1").style = "width: " + score + "%";
+    document.getElementById("reply1num").innerHTML = `${score}%`;
 }
