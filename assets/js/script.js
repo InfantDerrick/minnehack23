3a55975d5509d95675fbc59cef958a85d0836d14
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2023-01-29 14:25:59 -0600

    story

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,1 @@
-var options = {
-  strings: ["Welcome back to Civitas. \nYou have earned 32 more points since last time!\nThe museum restoration project requires your assistance."],
-  typeSpeed: 20,
-  showCursor: false,
-};
 
-var typed = new Typed('#typed', options);
