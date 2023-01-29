f6f6dcbdd092288b88aa3f8406589b13d4d25a4a
jeevanprakash0814 <jeevanpraks@gmail.com>
2023-01-29 14:38:37 -0600

    everything is working for the API

--- assets/js/google-fact-checker.js
+++ assets/js/google-fact-checker.js
@@ -2,7 +2,6 @@ function getFactCheck(query, callback = (printing) => {makeFactCheck(printing);}
     // fetch(`https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en-US&maxAgeDays=100&offset=0&query=${query}`)
     //     .then((response)) => response.json())
     //     .then((json) => console.log(json));
-    console.log(query);
     theUrl = `https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en-US&maxAgeDays=100&offset=0&query=${query}&key=AIzaSyBC0im9IZ8UfIFW9AkdW6q41XpCfL9z-CM`
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
@@ -15,12 +14,10 @@ function getFactCheck(query, callback = (printing) => {makeFactCheck(printing);}
 
 const makeFactCheck = (checks) => {
     const facts = JSON.parse(checks);
-    console.log(facts);
     var arr = [];
     if(facts['claims']){
         facts.claims.forEach((x) => {
             arr.push(x.claimReview[0].textualRating);
-            console.log(x.claimReview[0].textualRating);
         });
     }
     
@@ -29,25 +26,18 @@ const makeFactCheck = (checks) => {
     for(let i=0; i<arr.length; i++){
         counter = counter + 1;
         rating = arr[i].toLowerCase();
-        console.log(rating);
         if(rating.includes("true"))
             score = score + 1;
         else if(rating.includes("false")){
             score = score;
-            console.log("I AM HERE");
         }
         else
             score = score + 0.5;
-        console.log(score);
     }
 
-    console.log("Score" + score);
     score = parseFloat(score) / counter;
     score = score * 100;
     document.getElementById("reply1").style = "width: " + score + "%";
     document.getElementById("reply1num").innerHTML = `${score}%`;
-    // console.log(facts);
-    // const s = `<p id="articleinfo2">Article matched on: ${facts.claims[0].text}</p>`;
-    // console.log(s);
-    // document.getElementById("articleinfo2").innerText = s;
+    document.getElementById("articleinfo2").innerHTML = "<strong>" + "Most Relevant Article: " + facts.claims[0].text + "</strong>";
 }
